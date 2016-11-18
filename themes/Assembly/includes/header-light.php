64d2294aa7f48f7c1c05081793635cedfbcf82e1
Patrick Hoydar <phoydar@webmd.net>
2016-11-18 10:43:25 -0500

    numerous updates to base mobile (320) landing styles

--- themes/Assembly/includes/header-light.php
+++ themes/Assembly/includes/header-light.php
@@ -3,7 +3,7 @@
         <a href="/">
             <!-- logo SVG file -->
             <?php echo file_get_contents( get_template_directory_uri() . '/library/images/svg/full-logo.svg' ) ?>
-            <img class="menu-icon" src="<?php echo get_template_directory_uri() ?>/library/images/icons/menu.png" alt="">
         </a>
     </h1>
+    <a href="#"><img class="menu-icon mobile-only" src="<?php echo get_template_directory_uri() ?>/library/images/icons/mobile-menu-icon.png" alt=""></a>
 </header>
--- themes/Assembly/index.php
+++ themes/Assembly/index.php
@@ -2,7 +2,10 @@
 <div class="container">
 	<section class="landing">
 		<?php include 'includes/header-light.php'; ?>
+		<h2>here today. remembered tomorrow.</h2>
 		<div class="mesh-bg"></div>
+
+ 		<?php echo file_get_contents( get_template_directory_uri() . '/library/images/svg/down-arrow-mobile.svg' ) ?>
 	</section>
 	<section>
 
