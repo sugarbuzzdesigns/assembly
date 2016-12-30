07069e554fffbc14b38fa8dbae90c06c919e58b0
Patrick Hoydar <phoydar@webmd.net>
2016-12-29 22:54:07 -0500

    added video to about page

--- themes/Assembly/page-about-us.php
+++ themes/Assembly/page-about-us.php
@@ -4,6 +4,13 @@
 		<h1>about us</h1>
 		<div class="mesh-bg dark wide"></div>
 	</section>
-
+	<section class="videos">
+		<div class="video-wrapper">
+			<div class="video">
+				<img class="video-placeholder" src="<?php echo get_template_directory_uri(); ?>/library/images/placeholders/video-placeholder.jpg" alt="">
+				<?php include __DIR__ . '/library/images/svg/play-btn-square.svg'; ?>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
