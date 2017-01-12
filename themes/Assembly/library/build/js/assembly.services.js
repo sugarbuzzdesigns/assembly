859ee289d1ab85483de729cca7035ad4dad59366
Patrick Hoydar <phoydar@webmd.net>
2017-01-11 21:15:16 -0500

    case study page

--- themes/Assembly/library/build/js/assembly.services.js
+++ themes/Assembly/library/build/js/assembly.services.js
@@ -1,3 +1,4 @@
+/* repo: assembly/ - Package Version: 1.0.0 - 2017-01-11 03:22 pm - User: Phoydar */
 /*!
  * Assembly Menu Navigation
  */
--- themes/Assembly/page-case-studies.php
+++ themes/Assembly/page-case-studies.php
@@ -2,32 +2,39 @@
 <?php include 'includes/header-dark-interior.php'; ?>
 
 <section class="landing">
-	<div class="modular">
+	<div class="modular case-study-category">
+		<div class="hover-content">
+			<div class="white border square"></div>
+		</div>
 		<h3>
-			<span>m</span>
-			<span>o</span>
-			<span>d</span>
-			<span>u</span>
-			<span>l</span>
-			<span>a</span>
-			<span>r</span>
+			<span data-letter="m">m</span>
+			<span data-letter="o">o</span>
+			<span data-letter="d">d</span>
+			<span data-letter="u">u</span>
+			<span data-letter="l">l</span>
+			<span data-letter="a">a</span>
+			<span data-letter="r">r</span>
 		</h3>
-		<div class="white-square"></div>
+		<div class="white solid square"></div>
 	</div>
-	<div class="custom">
+	<div class="custom case-study-category">
+		<div class="hover-content">
+			<div class="white border square"></div>
+		</div>
 		<h3>
-			<span>c</span>
-			<span>u</span>
-			<span>s</span>
-			<span>t</span>
-			<span>o</span>
-			<span>m</span>
+			<span data-letter="c">c</span>
+			<span data-letter="u">u</span>
+			<span data-letter="s">s</span>
+			<span data-letter="t">t</span>
+			<span data-letter="o">o</span>
+			<span data-letter="m">m</span>
 		</h3>
-		<div class="white-square"></div>
+		<div class="white solid square"></div>
 	</div>
 </section>
 
 <section class="content">
+	<h2><span>custom</span><span>kits</span></h2>
 	<div class="filter-wrap">
 		<select class="select-filter">
 			<option value="all">All</option>
@@ -39,18 +46,97 @@
 			<a href="#" title="modular">M</a>
 		</div>
 	</div>
+	<div class="case-study-desc">
+		<h4>Custom Kits</h4>
+		<p>Lorem ipsum dolor sit amet, consf ectetur adipiscing elit. In tincidunt elementum orci in pretium. Mauris malesuada fermentum nisi vel lobortis heks.</p>
+		<p>tincidunt elementum orci in pretium. Mauris malesuada fermentum nisi vel lobortis.</p>
+	</div>
+	<div class="tile left-margin-med">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-1.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Super
+			</span>
+		</div>
+	</div>
+	<div class="tile left-margin-lrg">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-2.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Super
+			</span>
+		</div>
+	</div>
+	<div class="tile left-margin-sm">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-3.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Hospitality
+			</span>
+		</div>
+	</div>
+	<blockquote>
+		<strong>Lorem Ipsum</strong>
+		<p>Lorem ipsum dolor sit amet, consf ectetur adipiscing elit. In tincidunt elementum orci in pretium. Mauris malesuada ferme ntum nisi vel lobortis heks.</p>
+	</blockquote>
+	<div class="tile left-margin-med">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-4.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Sporting
+			</span>
+		</div>
+	</div>
+	<div class="tile left-margin-sm">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-5.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Hospitality
+			</span>
+		</div>
+	</div>
+	<div class="tile left-margin-lrg">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-6.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Experiential
+			</span>
+		</div>
+	</div>
+	<div class="tile left-margin-med">
+		<div class="tile-img">
+			<img src="<?php echo get_template_directory_uri(); ?>/library/images/pages/case-studies/case-studies-content-7.jpg" alt="">
+		</div>
+		<div class="tile-info">
+			<span class="tile-cat">
+				Experiential
+			</span>
+		</div>
+	</div>
 	<?php
-		$case_study_args = array('post_type'  => 'case_study');
-		$the_query = new WP_Query( $case_study_args );
+		// $case_study_args = array('post_type'  => 'case_study');
+		// $the_query = new WP_Query( $case_study_args );
 
-		if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
-			$cats = wp_get_post_terms(get_the_ID(), 'case_study_cat');
-			$tags = wp_get_post_terms(get_the_ID(), 'case_study_tag');
+		// if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
+		// 	$cats = wp_get_post_terms(get_the_ID(), 'case_study_cat');
+		// 	$tags = wp_get_post_terms(get_the_ID(), 'case_study_tag');
 
-			$taxonomy_names = get_post_taxonomies();
+		// 	$taxonomy_names = get_post_taxonomies();
 	?>
 
-	<div class="tile group2">
+<!-- 	<div class="tile group2">
 		<div class="tile-img">
 			<?php echo get_featured_image_html($post); ?>
 		</div>
@@ -59,9 +145,9 @@
 				<?php foreach ( $tags as $tag ) { echo $tag->name; } ?>
 			</span>
 		</div>
-	</div>
+	</div> -->
 
-	<?php endwhile; endif; wp_reset_postdata(); ?>
+	<?php //endwhile; endif; wp_reset_postdata(); ?>
 </section>
 
 <?php get_footer(); ?>
