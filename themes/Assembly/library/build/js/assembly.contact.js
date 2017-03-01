141d3b48a6a3bb46314536b714be6caca060267a
Patrick Hoydar <phoydar@webmd.net>
2017-02-28 21:21:35 -0500

    fixing revert

--- themes/Assembly/library/build/js/assembly.contact.js
+++ themes/Assembly/library/build/js/assembly.contact.js
@@ -1,4 +1,4 @@
-/* repo: assembly/ - Package Version: 1.0.0 - 2017-02-28 08:38 pm - User: Phoydar */
+/* repo: assembly/ - Package Version: 1.0.0 - 2017-02-28 09:20 pm - User: Phoydar */
 /*!
  * Assembly Contact Page
  */
