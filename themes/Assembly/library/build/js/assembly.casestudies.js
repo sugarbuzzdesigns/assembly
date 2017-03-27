ca71606190b3b48deffe4fdc499b8218f2eca177
Patrick Hoydar <phoydar@webmd.net>
2017-03-27 12:13:01 -0400

    case study spacing

--- themes/Assembly/library/build/js/assembly.casestudies.js
+++ themes/Assembly/library/build/js/assembly.casestudies.js
@@ -1,4 +1,4 @@
-/* repo: assembly/ - Package Version: 1.0.0 - 2017-03-27 11:42 am - User: Phoydar */
+/* repo: assembly/ - Package Version: 1.0.0 - 2017-03-27 12:08 pm - User: Phoydar */
 /*!
  * Assembly Menu Navigation
  */
