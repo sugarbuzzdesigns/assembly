a0983e9836799a0a293b7d9ad916bf36cd6ee717
Patrick Hoydar <phoydar@webmd.net>
2017-03-27 12:16:06 -0400

    plus box double fix

--- themes/Assembly/page-contact.php
+++ themes/Assembly/page-contact.php
@@ -68,18 +68,12 @@
 					<div class="image" data-photo-id="contact-photo-<?php echo $count; ?>">
 						<img src="<?php echo $img_dir . $photo['src']; ?>" alt="">
 						<div class="hover-overlay">
-							<svg class="plus-box" viewBox="71 106 46 46" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-								<defs>
-									<path id="a" d="M0 0h46v46H0z"/>
-									<mask id="b" x="0" y="0" width="46" height="46" fill="#fff">
-										<use xlink:href="#a"/>
-									</mask>
-								</defs>
-								<g fill="none" fill-rule="evenodd" transform="translate(71 106)">
-									<path class="x-path" d="M36.462 22.5H10.5M23 10.038V36" stroke="#000" stroke-linecap="square"/>
-									<use stroke="#313131" mask="url(#b)" stroke-width="2" xlink:href="#a"/>
-								</g>
-							</svg>
+							<div class="plus-box" viewBox="71 106 46 46" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<div class="close-x">
+									<span></span>
+									<span></span>
+								</div>
+							</div>
 						</div>
 					</div>
 					<?php endforeach; ?>
